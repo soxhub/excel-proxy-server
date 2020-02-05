dd43dacb9d1850aac1d2e686606ff669181691cf
Leah <leahlang4d@gmail.com>
2020-02-05 14:29:22 -0800

    updated request function

--- index.js
+++ index.js
@@ -1,71 +1,39 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const cors = require("cors");
+const util = require("util");
 const got = require("got");
-var cors = require("cors");
 const app = express();
 const port = 8080;
 
 app.use(cors());
+app.use(bodyParser.json());
 
-
-app.get("*", async (req, res) => {
+app.use("*", async (req, res) => {
+ 
   let url = req.query.targetUrl;
   let token = req.query.token;
+
+  let option = {
+    method: req.method,
+    headers: {
+      token: token
+    }
+  };
+
+  if(req.method === "PUT" || req.method === "POST"){
+    option.json = req.body;
+    option.responseType = "json";
+  }
+
   try {
-    let g = await got.get(url, {
-      headers: {
-        token: token
-      }
-    });
-    res.json(g.body);
+    let payload = await got(url, option);
+    return res.send(payload.body);
   } catch (error) {
-    res.json(error.response.body);
+    util.log(error.stack);
+    return res.send(error.stack);
   }
 });
 
-app.put("*", async (req, res) => {
-  let url = req.query.targetUrl;
-  let token = req.query.token;
-  try {
-    let pu = await got.put(url, {
-      headers: {
-        token: token
-      }
-    });
-    console.log(req.body);
-    res.json(pu.body);
-  } catch (error) {
-    res.json(error.response.body);
-  }
-});
-
-app.post("*", async (req, res) => {
-  let url = req.query.targetUrl;
-  let token = req.query.token;
-  try {
-    let p = await got.post(url, {
-      headers: {
-        token: token
-      }
-    });
-    res.json(p.body);
-  } catch (error) {
-    res.json(error.response.body);
-  }
-});
-
-app.delete("*", async (req, res) => {
-  let url = req.query.targetUrl;
-  let token = req.query.token;
-  try {
-    let d = await got.delete(url, {
-      headers: {
-        token: token
-      }
-    });
-    res.json(d.body);
-  } catch (error) {
-    res.json(error.response.body);
-  }
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
