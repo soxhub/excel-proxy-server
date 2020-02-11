87c3592fe7755db34d67067f62daf91d2a9d6b2f
Leah <leahlang4d@gmail.com>
2020-02-11 10:07:43 -0800

    add some debugging for heroku

--- middlewares/address.js
+++ middlewares/address.js
@@ -3,19 +3,17 @@ const requestIp = require('request-ip');
 
 function ipAddress() {
   return (req, res, next) => {
-    let ip = requestIp.getClientIp(req);
+    let ip = '104.61.86.43';
+    // requestIp.getClientIp(req);
+    console.log(requestIp.getClientIp(req))
 
     let addressSplit = ip.split(".");
     addressSplit.pop();
     let finalAddress = addressSplit.join(".");
 
-    console.log(req.ip);
-    conf
-      .get("ip_address")
-      .split(",")
-      .includes(finalAddress)
-      ? next()
-      : res.status(401).send({ message: "Unauthorized" });
+    console.log(conf.get("ip_address"));
+    console.log(conf.get("ip_address").split(","));
+    conf.get("ip_address").split(",").includes(finalAddress)? next(): res.status(401).send({ message: "Unauthorized" });
   };
 }
 
