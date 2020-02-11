b1195b8f110fb3255b14e83a66a0f2f2fbe263c5
Leah <leahlang4d@gmail.com>
2020-02-11 10:23:50 -0800

    clean up adress middleware

--- middlewares/address.js
+++ middlewares/address.js
@@ -3,14 +3,14 @@ const requestIp = require('request-ip');
 
 function ipAddress() {
   return (req, res, next) => {
+    
+    // Get the last three octects of ip address
     let ip = requestIp.getClientIp(req);
-    console.log(requestIp.getClientIp(req))
-
     let addressSplit = ip.split(".");
     addressSplit.pop();
     let finalAddress = addressSplit.join(".");
 
-    console.log('list of ip addresses ', conf.get("ip_address"));
+    // Check to see if incoming ip is in config vars
     conf.get("ip_address").includes(finalAddress)? next(): res.status(401).send({ message: "Unauthorized" });
   };
 }
