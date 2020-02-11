04f04c1982b8c182144fc78e3735c53ccacda15b
Leah <leahlang4d@gmail.com>
2020-02-11 09:55:06 -0800

    add request-ip console log

--- middlewares/address.js
+++ middlewares/address.js
@@ -1,15 +1,28 @@
-var conf = require('config');
+var conf = require("config");
+const requestIp = require('request-ip');
 
 function ipAddress() {
-    return (req, res, next) => {
-        let userAddress = req.ip;
-        let addressSplit = userAddress.split('.');
-        addressSplit.pop();
-        let finalAddress = addressSplit.join('.');
+  return (req, res, next) => {
+    // Check to see if users ip is in IPv6 format
+    let ip = req.ip;
+    if (ip.substr(0, 7) == "::ffff:") {
+      ip = ip.substr(7);
+    }
+    console.log(ip);
+    console.log(requestIp.getClientIp(req));
 
-        console.log(req.ip);
-        conf.get('ip_address').split(',').includes(finalAddress) ? next() : res.status(401).send({ message: 'Unauthorized' });
-    };
+    let addressSplit = ip.split(".");
+    addressSplit.pop();
+    let finalAddress = addressSplit.join(".");
+
+    console.log(req.ip);
+    conf
+      .get("ip_address")
+      .split(",")
+      .includes(finalAddress)
+      ? next()
+      : res.status(401).send({ message: "Unauthorized" });
+  };
 }
 
 module.exports = ipAddress;
