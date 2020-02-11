5297175d29d1fd7f790f02d7ce695ad36a28701b
Leah <leahlang4d@gmail.com>
2020-02-11 10:00:11 -0800

    change req.ip to requestIp

--- middlewares/address.js
+++ middlewares/address.js
@@ -3,13 +3,7 @@ const requestIp = require('request-ip');
 
 function ipAddress() {
   return (req, res, next) => {
-    // Check to see if users ip is in IPv6 format
-    let ip = req.ip;
-    if (ip.substr(0, 7) == "::ffff:") {
-      ip = ip.substr(7);
-    }
-    console.log(ip);
-    console.log(requestIp.getClientIp(req));
+    let ip = requestIp.getClientIp(req);
 
     let addressSplit = ip.split(".");
     addressSplit.pop();
