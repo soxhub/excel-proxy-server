7ce837a12fc3db48c91292e60794fec266428bc3
Leah <leahlang4d@gmail.com>
2020-02-11 10:13:20 -0800

    remove split, heroku converts config vars to array

--- middlewares/address.js
+++ middlewares/address.js
@@ -3,17 +3,15 @@ const requestIp = require('request-ip');
 
 function ipAddress() {
   return (req, res, next) => {
-    let ip = '104.61.86.43';
-    // requestIp.getClientIp(req);
+    let ip = requestIp.getClientIp(req);
     console.log(requestIp.getClientIp(req))
 
     let addressSplit = ip.split(".");
     addressSplit.pop();
     let finalAddress = addressSplit.join(".");
 
-    console.log(conf.get("ip_address"));
-    console.log(conf.get("ip_address").split(","));
-    conf.get("ip_address").split(",").includes(finalAddress)? next(): res.status(401).send({ message: "Unauthorized" });
+    console.log('list of ip addresses ', conf.get("ip_address"));
+    conf.get("ip_address").includes(finalAddress)? next(): res.status(401).send({ message: "Unauthorized" });
   };
 }
 
