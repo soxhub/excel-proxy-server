52cde008d8c1ad24c1eff663ae98ef6b343c4300
Leah <leahlang4d@gmail.com>
2020-03-03 20:53:12 -0800

    add dropzone uploder

--- frontend/src/components/Uploader.js
+++ frontend/src/components/Uploader.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useAlert } from 'react-alert';
+import { useDropzone } from 'react-dropzone';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,6 +10,7 @@ function Upload() {
   let [token, setToken] = useState(null);
   let [url, setUrl] = useState(null);
   let [uploading, setUploading] = useState(null);
+  let [ fileName, setFileName ] = useState(null);
 
   const onFormSubmit = e => {
     e.preventDefault()
@@ -21,7 +23,7 @@ function Upload() {
     formData.append('url', url);
     formData.append('token', token);
 
-    const startAlert = alert.show('Narrative Being uploaded', {type: 'info', timeout: 3000000});
+    const startAlert = alert.show('Narrative Being uploaded', { type: 'info', timeout: 3000000 });
     axios.post('/api/upload', formData, startAlert)
       .then(() => {
         alert.show('Narrative successfully uploaded', {
@@ -45,7 +47,7 @@ function Upload() {
 
   const validated = () => {
     let errorMsg = ''
-
+    
     if (token === '' || url === '') {
       // this case might not be needed anymore
       errorMsg = 'Please enter valid credentials and client app url.';
@@ -67,59 +69,95 @@ function Upload() {
 
   const onChange = event => {
     setUploading(event.target.files[0])
+    setFileName(event.target.files[0].name);
   }
+
+  const onDropAccepted = (file) => {
+    if (file.length > 1) {
+      alert.show('You can only upload one zip file', { type: 'error' });
+    } else {
+      setFileName(null);
+      setUploading(file[0]);
+    }
+  }
+
+
+  const { getRootProps, getInputProps, open, acceptedFiles } = useDropzone({
+    noClick: true,
+    noKeyboard: true,
+    accept: 'application/zip',
+    onDrop: onDropAccepted
+  });
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+      </li>
+  ));
+
+
 
   let disableSubmit = !url || !token || !uploading;
 
   return (
-    <form className="panel-body form-theme-2 npb">
-      <div className="form-group">
-        <input
-          type="url"
-          name="instanceUrl"
-          className="form-control input-sm"
-          onChange={e => setUrl(e.target.value)}
-          placeholder="Instance URL"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <input
-          type="password"
-          name="instanceToken"
-          className="form-control input-sm"
-          onChange={e => setToken(e.target.value)}
-          placeholder="Token"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <div className="row">
-          <div className="col-sm">
-            <input
-              style={{ width: 300, marginTop: 20 }}
-              type="file"
-              onChange={onChange}
-              encType="multipart/form-data"
-              className="btn btn-primary btn-md btn-big-txt "
-              name="myNarrative"
-              accept=".zip"
-            ></input>
-          </div>
-          <div className="col-sm">
-            <button
-              style={{ float: 'right', marginTop: 20}}
-              type="submit"
-              onClick={onFormSubmit}
-              disabled={disableSubmit}
-              className="btn btn-primary btn-center btn-lg btn-big-txt"
-            >
-              Upload
-            </button>
-          </div>
-        </div>
-      </div>
-    </form>
+		<form className="panel-body form-theme-2 npb">
+			<div className="form-group">
+				<label>INSTANCE URL</label>
+				<input
+					type="url"
+					name="instanceUrl"
+					className="form-control input-sm"
+					onChange={e => setUrl(e.target.value)}
+					required
+				/>
+			</div>
+			<div className="form-group">
+				<label>TOKEN</label>
+				<input
+					type="password"
+					name="instanceToken"
+					className="form-control input-sm"
+					onChange={e => setToken(e.target.value)}
+					required
+				/>
+			</div>
+			<div className="drop-container">
+				<div {...getRootProps({ className: 'dropzone' })}>
+					<input
+						{...getInputProps()}
+						onChange={onChange}
+						encType="multipart/form-data"
+						name="myNarrative"
+						accept=".zip"
+					/>
+					<button
+						className="btn btn-primary btn-md browse btn-big-txt"
+						type="button"
+						onClick={open}
+					>
+						Browse Files
+					</button>
+					<p>or drop a file to upload</p>
+					<em>(Only .zip files will be accepted)</em>
+				</div>
+        {fileName}
+				{files.length === 1 && <ul>{files}</ul>}
+			</div>
+			<div className="form-group">
+				<div className="row">
+					<div className="col-sm">
+						<button
+							style={{ float: 'right', marginTop: 20 }}
+							type="submit"
+							onClick={onFormSubmit}
+							disabled={disableSubmit}
+							className="btn btn-primary btn-center btn-lg btn-big-txt"
+						>
+							Upload
+						</button>
+					</div>
+				</div>
+			</div>
+		</form>
   )
 }
 
